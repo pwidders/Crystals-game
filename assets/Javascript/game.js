540ef1925d3ed4a2905bec8330e621895f7d382f
Pete Widders <pwidders@hotmail.com>
2018-11-12 13:08:34 -0800

    Got the first click random value to generate and appear in the click-score box. Will use this process for all four Crystal buttons.

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -3,25 +3,72 @@ $(document).ready(function() {
     // Your code here.
 
     // Reference DOM elements
-    var _randomNumber = document.getElementById('random-number'); 
+    var _randomNumber = document.getElementById("random-number"); 
+
+    var _CrystalValueRuby = document.getElementById("ruby");
+
+    var _scoreBoardBox = document.getElementById("scoreBoard");
+
+    var _totalScore = document.getElementById("total-score");
 
     //define variables
     var minNumber = 19;
     
     var maxNumber = 120;
+
+    var crystalMin = 1;
+
+    var crystalMax = 12;
     
     var chosenNumber = randomNumberFromRange(minNumber, maxNumber);
+
+    var crystalValue = randomNumberFromRange(crystalMin, crystalMax);
+
+    
 
     // Generate random number between 19-120 when page is loaded. 
 
     function randomNumberFromRange (min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
+    {
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
+    
     $(_randomNumber).append("<b>Your target number is: </b>" + (chosenNumber));
 
+    // Create an onclick event that generates random value for each crystal of 1-12 and adds that value to total score
+    
+        //Ruby
+    $(_CrystalValueRuby).click(function() {
+        function randomNumberFromRange (min,max)
+        {
+        return Math.floor(Math.random()*(max-min+1)+min);
+        } 
+        $(_scoreBoardBox).replaceWith("<h2>" + (crystalValue) + "</h2>");
+    })
+        //
+    $(_CrystalValueOne).click(function() {
+        function randomNumberFromRange (min,max)
+        {
+        return Math.floor(Math.random()*(max-min+1)+min);
+        } 
+        $(_scoreBoardBox).replaceWith("<h2>" + (crystalValue) + "</h2>");
+    })
+    
 
+    console.log(_scoreBoardBox);
+    
+        //$("#ruby").click(function(){
+        //    $(_scoreBoardBox).val(crystalValueA);
+        //    $("#scoreBoard").append(crystalValueA);
+        //})
+
+    
+
+    console.log(_scoreBoardBox);
+    
     // Player clicks a crystal containing a random number. That number gets added to player's score.
+
+        //
 
     // player's score === random number = win!
 
